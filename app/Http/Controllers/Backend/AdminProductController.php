32a46f2f7477200ad5d66c74eeda669e277bd974
Amanullah Nilger <nilgeramanullah@gmail.com>
2024-01-22 10:18:26 +0300

    validation commit

--- app/Http/Controllers/Backend/AdminProductController.php
+++ app/Http/Controllers/Backend/AdminProductController.php
@@ -10,14 +10,8 @@ class AdminProductController extends Controller
 {
     public function store(Request $request)
     {
-        // $request->validate([
-        //     "name"=>"required|max:255",
-        //     "description"=>"required",
-        //     "price"=>"required|numeric|gt:0",
-        //     "image"=>"image",
-        // ]);
-      
         Product::validate($request);
+
         $newProduct = new Product();
 
         $newProduct->name = $request->name;
@@ -64,12 +58,7 @@ class AdminProductController extends Controller
 
   public function update(Request $request,$id)
   {
-    //  $request->validate([
-    //    "name"=>"required|max:255",
-    //    "description" => "required",
-    //    "price" => "required|numeric|gt:0",
-    //    "image" => "image",
-    //  ]);
+
     Product::validate($request);
 
      $product = Product::findOrFail($id);
