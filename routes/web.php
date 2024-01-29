6ee27655a6f940df7927347753cbb5c99e65d660
Amanullah Nilger <nilgeramanullah@gmail.com>
2024-01-30 00:12:50 +0300

    admin middleware commit

--- routes/web.php
+++ routes/web.php
@@ -24,13 +24,15 @@ Route::get('/','App\Http\Controllers\Frontend\HomeController@index')->name('inde
 Route::get('/about','App\Http\Controllers\Frontend\HomeController@about')->name('about-us');
 Route::get('/product','App\Http\Controllers\Frontend\ProductController@index')->name('product.index');
 Route::get('/product/{id}','App\Http\Controllers\Frontend\ProductController@show')->name('products.show');
-Route::get('/admin','App\Http\Controllers\Backend\AdminHomeController@index')->name('admin.home.index');
-Route::get('/admin/products','App\Http\Controllers\Backend\AdminProductController@index')->name('admin.product.index');
-Route::post('/admin/products/store','App\Http\Controllers\Backend\AdminProductController@store')->name('admin.product.store');
-Route::delete('/admin/products/{id}/delete','App\Http\Controllers\Backend\AdminProductController@delete')->name('admin.product.delete');
-Route::get('/admin/products/{id}/edit','App\Http\Controllers\Backend\AdminProductController@edit')->name('admin.product.edit');
-Route::put('/admin/products/{id}/update','App\Http\Controllers\Backend\AdminProductController@update')->name('admin.product.update');
 
+Route::middleware('admin')->group(function (){
+    Route::get('/admin','App\Http\Controllers\Backend\AdminHomeController@index')->name('admin.home.index');
+    Route::get('/admin/products','App\Http\Controllers\Backend\AdminProductController@index')->name('admin.product.index');
+    Route::post('/admin/products/store','App\Http\Controllers\Backend\AdminProductController@store')->name('admin.product.store');
+    Route::delete('/admin/products/{id}/delete','App\Http\Controllers\Backend\AdminProductController@delete')->name('admin.product.delete');
+    Route::get('/admin/products/{id}/edit','App\Http\Controllers\Backend\AdminProductController@edit')->name('admin.product.edit');
+    Route::put('/admin/products/{id}/update','App\Http\Controllers\Backend\AdminProductController@update')->name('admin.product.update');    
+}); 
 Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
