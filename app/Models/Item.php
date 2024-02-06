8d0660a8d5226e9ed1509e51f30f248692db947a
Amanullah Nilger <nilgeramanullah@gmail.com>
2024-02-06 17:03:58 +0300

    item commit

---
+++ app/Models/Item.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Order;
+use App\Models\Product;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    public static function validate(Request $request)
+    {
+        $request->validate([
+            "price" => "required|numeric|gt:0",
+            "quantity" => "required|numeric|gt:0",
+            "product_id" => "required|exists:products,id",
+            "order_id" => "required|exists:orders,id",
+        ]);
+    }
+
+
+}
---
+++ app/Models/Order.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Item;
+
+class Order extends Model
+{
+    // use HasFactory;
+
+
+    public static function validate($request)
+    {
+        $request->validate([
+            "total" =>  "required|numeric",
+            "user_id" => "required|exists:user,id",
+        ]);       
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
+
+    protected $fillable =  ['total','user_id','user','items'];
+    
+}
